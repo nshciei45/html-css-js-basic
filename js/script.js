165fe203411405193079a5296d35f2eeb6be740c
Pine <ns.h32ab@gmail.com>
2020-08-17 21:24:23 +0630

    m

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",function(event){
+document.querySelector("#btn2").addEventListener("click",
+		function(event){
+			document.querySelector("#btn2").textContent="clicked";
+		}
+);
+});
